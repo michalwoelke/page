dd884ce4b0e5c84df1a4fddfeda6cee4e44ff6b0
MichaelPogU <83838717+MichaelPogU@users.noreply.github.com>
2023-06-18 01:29:32 +0200

    add files

---
+++ config.js
@@ -0,0 +1,210 @@
+
+const CONFIG = {
+	// General
+	name: 'Michał',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://gmail.com/',
+		},
+		{
+			id: '3',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com/',
+		},
+		{
+			id: '4',
+			name: 'YouTube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
+		},
+		{
+			id: '5',
+			name: 'messenger',
+			icon: 'message-circle',
+			link: 'https://messenger.com/',
+		},
+		{
+			id: '6',
+			name: 'Twitch',
+			icon: 'twitch',
+			link: 'https://twitch.tv/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'graduation-cap',
+			id: '1',
+			links: [
+				{
+					name: 'Arkusze Maturalne',
+					link: 'https://arkusze.pl/',
+				},
+				{
+					name: 'w3schools',
+					link: 'https://www.w3schools.com/',
+				},
+				{
+					name: 'Karta Wzorów',
+					link: 'https://arkusze.pl/informatory/wzory-matematyczne-2023.pdf',
+				},
+				{
+					name: 'Odrabiamy',
+					link: 'https://odrabiamy.pl/moje',
+				},
+			],
+		},
+		{
+			icon: 'clapperboard',
+			id: '2',
+			links: [
+				{
+					name: 'Piratebay',
+					link: 'https://thepiratebay.org/',
+				},
+				{
+					name: 'loader',
+					link: 'https://en.loader.to/4/',
+				},
+				{
+					name: 'imdb',
+					link: 'https://www.imdb.com/',
+				},
+				{
+					name: 'Helios',
+					link: 'https://www.helios.pl/50,Poznan/Repertuar/',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
